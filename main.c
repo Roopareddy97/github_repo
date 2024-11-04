2db1881bbc43792eea038482f4db6543cc3b4ece
Roopareddy97 <roopareddydmr97@gmail.com>
2024-11-04 14:07:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
-ABC.exe:main.o fact.o big2.o
-  gcc -o ABC.exe main.o fact.o big2.o
-main.o:main.c
-  gcc -c main.c
-fact.o:fact.c
-  gcc -c fact.c
-big2.o:big2.c
-  gcc -c big2.c
+#include<studio.h>
+main()
+{
+factorial();
+biggest2();
+}
